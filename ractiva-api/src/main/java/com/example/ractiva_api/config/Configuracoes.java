b9afc683392096f525aaba6ac026f229d0608416
Guiherme <guilherme.shizuru@gmail.com>
2025-05-29 18:52:46 -0300

    refactor/ajuste de url

--- ractiva-api/src/main/java/com/example/ractiva_api/config/Configuracoes.java
+++ ractiva-api/src/main/java/com/example/ractiva_api/config/Configuracoes.java
@@ -1,6 +1,5 @@
 package com.example.ractiva_api.config;
 
 public class Configuracoes {
-    public static String KEY_GEMINI = "";
-    public static String URL_GEMINI = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent";
+    public static String URL_GEMINI = "https://generativelanguage.googleapis.com/v1beta/models/{model}:generateText";
 }
