35d61a066834e7319f12dfbdbc2a7bc3774fbcda
Guiherme <guilherme.shizuru@gmail.com>
2025-05-29 18:52:05 -0300

    feature/Ajuste para comunicacao com front

--- ractiva-api/src/main/java/com/example/ractiva_api/controller/ReactivaController.java
+++ ractiva-api/src/main/java/com/example/ractiva_api/controller/ReactivaController.java
@@ -3,14 +3,12 @@ package com.example.ractiva_api.controller;
 import com.example.ractiva_api.Model.PromptRequest;
 import com.example.ractiva_api.service.GeminiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("api/carta")
+@RequestMapping("api/reactiva")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReactivaController {
 
     @Autowired
