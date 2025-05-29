ab17e2f0082b4853e86fbbc302e34a67a37da2a8
Guiherme <guilherme.shizuru@gmail.com>
2025-05-29 18:01:07 -0300

    feature/criacao do objeto prompt

---
+++ ractiva-api/src/main/java/com/example/ractiva_api/Model/PromptRequest.java
@@ -0,0 +1,12 @@
+package com.example.ractiva_api.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PromptRequest {
+    private String prompt;
+}
