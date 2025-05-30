84feb7c0ed947e002f0a5e3806eeb164c37ff0ee
Guiherme <guilherme.shizuru@gmail.com>
2025-05-29 23:00:32 -0300

    Refactor/ refatoracao total de classe

--- ractiva-api/src/main/java/com/example/ractiva_api/service/GeminiService.java
+++ ractiva-api/src/main/java/com/example/ractiva_api/service/GeminiService.java
@@ -1,93 +1,73 @@
 package com.example.ractiva_api.service;
 
-import com.example.ractiva_api.config.Configuracoes;
-import com.example.ractiva_api.model.Carta;
+import com.example.ractiva_api.config.GeminiConfig;
+import com.example.ractiva_api.exception.GeminiApiException;
+import com.example.ractiva_api.model.modelRequest.ConteudoRequest;
+import com.example.ractiva_api.model.modelRequest.GeminiRequest;
+import com.example.ractiva_api.model.modelRequest.PartesRequest;
+import com.example.ractiva_api.model.modelResponse.GeminiReponseHandler;
+import com.example.ractiva_api.model.modelResponse.GeminiResponse;
 import com.example.ractiva_api.repository.CartaRepository;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class GeminiService {
-
-    @Value("${gemini.api.key}")
-    private String apiKey;
-
-    private static final Logger logger = LoggerFactory.getLogger(GeminiService.class);
 
     @Autowired
     private CartaRepository cartaRepository;
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public String gerarCarta(String prompt) {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private GeminiConfig configuracoesGemini;
+
+    @Autowired
+    private GeminiReponseHandler geminiReponseHandler;
+
+    @Autowired
+    private CartaBuilderService cartaBuilderService;
+
+
+    public String gerarCarta(String prompt) throws JsonProcessingException {
+
+        GeminiRequest request = new GeminiRequest(
+                List.of(new ConteudoRequest(
+                        List.of(new PartesRequest(
+                                prompt)))
+        ));
+
+        String jsonBody = objectMapper.writeValueAsString(request);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                configuracoesGemini.getUrlSecret(),
+                HttpMethod.POST,
+                entity,
+                String.class
+        );
+
         try {
-            String escapedPrompt = objectMapper.writeValueAsString(prompt);
+            GeminiResponse geminiResponse = geminiReponseHandler.validarEConverter(response);
+            String textoGerado = geminiReponseHandler.extrairTexto(geminiResponse);
+            cartaBuilderService.criarCarta(prompt, textoGerado);
 
-            String jsonBody = String.format("""
-                    {
-                      "contents": [
-                        {
-                          "parts": [
-                            {
-                              "text": %s
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                    """, escapedPrompt);
+            return textoGerado;
 
-            String url = Configuracoes.URL_GEMINI + "/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey;
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
-            HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
-
-            ResponseEntity<String> response = restTemplate.exchange(
-                    url,
-                    HttpMethod.POST,
-                    entity,
-                    String.class
-            );
-
-            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-                logger.info("Resposta crua da Gemini API:\n{}", response.getBody());
-
-                JsonNode root = objectMapper.readTree(response.getBody());
-                String textoGerado = root.path("candidates")
-                        .get(0)
-                        .path("content")
-                        .path("parts")
-                        .get(0)
-                        .path("text")
-                        .asText();
-
-                Carta carta = Carta.builder()
-                        .prompt(prompt)
-                        .resposta(textoGerado)
-                        .criadoEm(System.currentTimeMillis())
-                        .build();
-
-                cartaRepository.save(carta);
-
-                return textoGerado;
-            } else {
-                logger.error("Erro na chamada da Gemini API: Status {} - Body {}", response.getStatusCode(), response.getBody());
-                throw new RuntimeException("Erro na chamada da Gemini API: " + response.getBody());
-            }
         } catch (Exception e) {
-            logger.error("Erro ao gerar carta com Gemini", e);
-            throw new RuntimeException("Erro ao gerar carta com Gemini", e);
+            throw new GeminiApiException();
         }
     }
 }
